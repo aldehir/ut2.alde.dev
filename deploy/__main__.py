8e77f2cbf43550d67f38b44de58902d79edee8d7
Alde Rojas <hello@alde.dev>
2023-09-05 19:20:41 -0500

    Run dedicated instance

--- deploy/__main__.py
+++ deploy/__main__.py
@@ -17,7 +17,7 @@ if stack == "production":
 
 elif stack == "dev":
     servers += [
-        create_server(image.id, 1, "us-ord")
+        create_server(image.id, 1, "us-ord", "g6-dedicated-2")
     ]
 
 pulumi.export("servers", [generate_export(x) for x in servers])
