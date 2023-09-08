6a31aa6d568f1aa9da86f10f646842137d9a0834
Alde Rojas <hello@alde.dev>
2023-09-08 02:59:06 -0500

    Add a DO instance

--- deploy/__main__.py
+++ deploy/__main__.py
@@ -4,7 +4,7 @@ import pulumi_cloudflare as cloudflare
 
 import typing
 
-from gameservers import UT2GameServerLinode
+from gameservers import UT2GameServerLinode, UT2GameServerDigitalOcean
 
 config = pulumi.Config()
 stack = pulumi.get_stack()
@@ -22,28 +22,32 @@ assert deployment
 instances = []
 
 for provider, provider_config in deployment.items():
-    if provider == "linode":
-        base = provider_config["base"]
+    Server = UT2GameServerLinode
+    if provider == "digitalocean":
+        Server = UT2GameServerDigitalOcean
 
-        for region_id, region_config in provider_config["regions"].items():
-            region_name = region_config["name"]
+    base = provider_config["base"]
 
-            for i, instance_type in enumerate(region_config["instances"], 1):
-                instance_name = f"ut2.{region_name}-{i}.{domain}"
+    for region_id, region_config in provider_config["regions"].items():
+        region_name = region_config["name"]
 
-                s = UT2GameServerLinode(
-                    instance_name,
-                    region=region_id,
-                    type=instance_type,
-                    zone_name=zone_name,
-                    server_name=instance_name,
-                    image=base,
-                    tags=[
-                        "ut2-server",
-                        f"ut2-server-{environment}"
-                    ],
-                )
+        for i, instance_type in enumerate(region_config["instances"], 1):
+            instance_name = f"ut2.{region_name}-{i}.{domain}"
 
-                instances.append(s)
+            s = Server(
+                instance_name,
+                region=region_id,
+                type=instance_type,
+                zone_name=zone_name,
+                server_name=instance_name,
+                image=base,
+                tags=[
+                    "ut2-server",
+                    f"ut2-server-{environment}"
+                ],
+            )
+
+            instances.append(s)
+
 
 pulumi.export("instances", [{"name": x.server_name, "ip": x.ip_address, "tags": x.tags} for x in instances])
--- deploy/gameservers.py
+++ deploy/gameservers.py
@@ -2,6 +2,7 @@ import pulumi
 from pulumi.resource import ResourceOptions
 import pulumi_linode as linode
 import pulumi_cloudflare as cloudflare
+import pulumi_digitalocean as digitalocean
 
 
 class UT2GameServerLinode(pulumi.ComponentResource):
@@ -46,3 +47,46 @@ class UT2GameServerLinode(pulumi.ComponentResource):
         self.ip_address = inst.ip_address
 
         self.register_outputs({"ipAddress": inst.ip_address})
+
+
+class UT2GameServerDigitalOcean(pulumi.ComponentResource):
+    def __init__(
+        self,
+        name,
+        opts=None,
+        region: str = "nyc1",
+        type: str = "s-vcpu-1gb",
+        zone_name: str = "",
+        server_name: str = "",
+        image: str = "",
+        tags: list[str] = [],
+    ):
+        super().__init__("pkg:index:UT2GameServerDigitalOcean", name, None, opts)
+
+        self.server_name = server_name
+        self.tags = tags
+
+        zone = cloudflare.get_zone(name=zone_name)
+
+        inst = digitalocean.Droplet(
+            f"{name}-instance",
+            name=server_name,
+            size=type,
+            region=region,
+            image=image,
+            tags=tags,
+            opts=pulumi.ResourceOptions(parent=self),
+        )
+
+        cloudflare.Record(
+            f"{name}-record",
+            zone_id=zone.id,
+            name=server_name,
+            type="A",
+            value=inst.ipv4_address,
+            opts=ResourceOptions(parent=self),
+        )
+
+        self.ip_address = inst.ipv4_address
+
+        self.register_outputs({"ipAddress": inst.ipv4_address})
